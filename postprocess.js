0a06e90add538aa209c8a334d0e6dbe6b96f7480
Javier Arce <4933+javierarce@users.noreply.github.com>
2021-10-05 22:17:11 +0200

    Postprocess

---
+++ postprocess.js
@@ -0,0 +1,40 @@
+import { readJSON, readCSV, writeCSV, writeJSON, removeFile } from 'https://deno.land/x/flat@0.0.11/mod.ts' 
+
+//const json = await readJSON('stations.json')
+
+let data = await readCSV('data.csv')
+const filename = Deno.args[0] 
+const json = await readJSON(filename)
+
+const magic = (station) => {
+  let id = station.id
+  let available = station.no_available ? 0 : 1
+  let light = station.light
+  let bases = station.total_bases
+  let bikes = station.dock_bikes
+  let free_bases = station.free_bases
+  let reservations = station.reservations_count
+  let now = new Date(json.updated_at)
+
+  let day = now.getDate()
+  let month = now.getMonth() + 1
+
+  let dd = day < 10 ? `0${day}` : day
+  let mm = month < 10 ? `0${month}` : month
+  let yyyy = now.getFullYear()
+
+  let date = `${yyyy}/${mm}/${dd}`
+  let time = now.getHours()
+
+  return { date, time, id, available,bases, bikes, free_bases, reservations, light }
+}
+
+Object.values(json.stations).forEach(async (station) => {
+  let row = magic(station)
+  //console.log(data)
+  data.push(row)
+  await writeCSV('data.csv', data) 
+})
+
+//console.log("Wrote a post process file")
+//await removeFile('./btc-price.json') // equivalent to removeFile('btc-price.json')
